377fab285bd60beb65326216412df7c29e6a39fa
Magiduck <tijsvanlieshout@gmail.com>
2019-03-25 18:07:53 +0100

    install pyfirmata if user has not installed it yet

--- sensor_data_collector.py
+++ sensor_data_collector.py
@@ -1,4 +1,10 @@
-from pyfirmata import Arduino, util
+try:
+    from pyfirmata import Arduino, util
+except:
+    import pip
+    pip.main(['install', "pyfirmata"])
+    from pyfirmata import Arduino, util
+
 import time
 
 
@@ -10,28 +16,43 @@ def main():
     red_led = board.get_pin('d:6:o')
     blue_led = board.get_pin('d:10:o')
     temp_sensor = board.get_pin('a:0:i')
+    temp_sensor.enable_reporting()
     photo_sensor = board.get_pin('a:2:i')
+    photo_sensor.enable_reporting()
+    center_button = board.get_pin('d:13:0')
+    center_button.enable_reporting()
+
+    previous_button_value = 0
+
+    is_reading_values = False
 
     time.sleep(1)  # VERY IMPORTANT, NEEDS TO BE INCLUDED AT THE BEGINNING
-    photo_value = photo_sensor.read()
-    temp_value = temp_sensor.read()
-    print(f"Photo sensor value: {photo_value}")
-    print(f"Photo sensor value in lux: {photo_value}")
-    volt = 4.98
-    degrees_celsius = ((temp_value*volt)/0.01)-273.15
-    print(f"Temperature sensor value: {temp_value}")
-    print(f"Temperature sensor value in degrees Celsius: {degrees_celsius}")
+    print("Ready!")
+    while True:
+        center_button_value = center_button.read()
+        is_reading_values = set_reading_values(is_reading_values, center_button_value, previous_button_value)
 
-    # loop_times = input('How many times would you like the LED to blink: ')
-    #
-    # print('Blinking ' + loop_times + ' times.')
-    #
-    # time.sleep(1.0)
-    # for x in range(int(loop_times)):
-    #     digital_6.write(1)
-    #     time.sleep(0.5)
-    #     digital_6.write(0)
-    #     time.sleep(0.5)
+        if is_reading_values:
+            print(is_reading_values)
+            red_led.write(1)
+            photo_value = photo_sensor.read()
+            temp_value = temp_sensor.read()
+            print(f"Photo sensor value: {photo_value}")
+            print(f"Photo sensor value in lux: {photo_value}")
+            volt = 4.98
+            degrees_celsius = ((temp_value * volt) / 0.01) - 273.15
+            print(f"Temperature sensor value: {temp_value}")
+            print(f"Temperature sensor value in degrees Celsius: {degrees_celsius}")
+            red_led.write(0)
+        previous_button_value = center_button_value
+
+
+def set_reading_values(is_reading_values, center_button_value, previous_button_value):
+    if center_button_value == 1 and previous_button_value == 0:
+        if is_reading_values:
+            return False
+        else:
+            return True
 
 
 if __name__ == '__main__':
