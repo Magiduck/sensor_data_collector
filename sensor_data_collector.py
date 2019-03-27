02170e0e74cd7daf8224d646e751c67b1b510b9f
Magiduck <tijsvanlieshout@gmail.com>
2019-03-27 21:28:40 +0100

    added documentation to code

--- sensor_data_collector.py
+++ sensor_data_collector.py
@@ -6,25 +6,28 @@ except:
     pip.main(['install', "pyfirmata"])
     from pyfirmata import Arduino, util
 
-from docopt import docopt
+from docopt import docopt  # Library for CLI
 import time
 
-from MicroController import MicroController
+from MicroController import MicroController  # Custom class to hold info of the arduino
 
+# Command line options / menu
 usage = """
 Usage:
     sensor_data_collector.py 
-    sensor_data_collector.py start <interval>
+    sensor_data_collector.py start <interval_in_seconds>
 """
 
 args = docopt(usage)
 
 
 def main():
+    # Pyfirmata
     board = Arduino("COM4")
     it = util.Iterator(board)
     it.start()
 
+    # Setting up pins on the Arduino board
     red_led = board.get_pin('d:6:o')
     blue_led = board.get_pin('d:10:o')
     temp_sensor = board.get_pin('a:0:i')
@@ -35,40 +38,51 @@ def main():
     top_button = board.get_pin('d:12:i')
     top_button.enable_reporting()
 
+    # Starting values
     is_outputting_photo = False
-
     time_start = time.time()
     debounce_start = time.time()
 
+    # Custom object to store info of the Arduino micro controller
     micro_controller = MicroController(center_button, debounce_start, is_outputting_photo, photo_sensor, temp_sensor,
                                        time_start, red_led, blue_led, args)
 
-    time.sleep(1)  # VERY IMPORTANT, NEEDS TO BE INCLUDED AT THE BEGINNING
+    time.sleep(1)  # Important for pyfirmata to initialize before trying to read values
     print("Ready!")
-    if args['start'] or top_button.read() == 1:
-        while True:
-            read_data(micro_controller)
+    if args['start']:  # If the argument start was given
+        while True:  # Infinite loop for not finishing the program but waiting on Arduino inputs
+            read_data(micro_controller)  # Read the data from the Arduino
 
 
 def read_data(micro_controller):
+    """ Reading light sensor and temperature sensor data from the Arduino."""
+
     center_button_value = micro_controller.center_button.read()
-    # print(f"center_button_value: {center_button_value}")
+
+    # Check if the button has been pressed, keeping in mind the debounce
     if center_button_value != 0 and time.time() - micro_controller.debounce_start > 0.2:
+        # Set boolean to true or false based on if photo sensor should be outputting or temperature sensor
         micro_controller.set_outputting_photo(set_outputting_photo(micro_controller.is_outputting_photo,
                                                                    center_button_value))
+        # Set the debounce again for the next loop
         micro_controller.set_debounce_start(time.time())
 
+    # Read values of both sensors
     photo_value = micro_controller.photo_sensor.read()
     temp_value = micro_controller.temp_sensor.read()
 
+    # Converting the raw output of the temp sensor into degrees celsius
     volt = 4.98
     degrees_celsius = ((temp_value * volt) / 0.01) - 273.15
     degrees_celsius = round(degrees_celsius, 2)
 
+    # Print the data based on which sensor should be outputted
     print_data(micro_controller, photo_value, temp_value, degrees_celsius)
 
 
 def set_outputting_photo(is_outputting_photo, center_button_value):
+    """ Determining if the photo sensor or temperature sensor should be outputted."""
+    # Just a simple switch when the button has been pressed
     if center_button_value == 1:
         if is_outputting_photo:
             return False
@@ -77,14 +91,17 @@ def set_outputting_photo(is_outputting_photo, center_button_value):
 
 
 def print_data(micro_controller, photo_value, temp_value, degrees_celsius):
-    if time.time() - micro_controller.time_start > float(args['<interval>']):
+    """ Print the data from either the photo sensor or temperature sensor based on set_outputting_photo."""
+    # Only continue if the user-specified interval (in seconds) has passed
+    if time.time() - micro_controller.time_start > float(args['<interval_in_seconds>']):
+        # Set start time for next loop
         micro_controller.set_time_start(time.time())
-        if micro_controller.is_outputting_photo:
+        if micro_controller.is_outputting_photo:  # Output the data from the photo sensor
             micro_controller.red_led.write(1)
             print(f"{time.strftime('%a %H:%M:%S')} - {photo_value} - {photo_value}")
             micro_controller.red_led.write(0)
 
-        else:
+        else:  # Output the data from the temperature sensor
             micro_controller.blue_led.write(1)
             print(f"{time.strftime('%a %H:%M:%S')} - {temp_value} - {degrees_celsius}")
             micro_controller.blue_led.write(0)
