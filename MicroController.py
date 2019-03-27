c4bbfe13453d8336e4a88d9c3ae431c95e80e162
Magiduck <tijsvanlieshout@gmail.com>
2019-03-27 20:14:51 +0100

    custom MicroController class to hold data and cleanup code

---
+++ MicroController.py
@@ -0,0 +1,22 @@
+class MicroController:
+
+    def __init__(self, center_button, debounce_start, is_outputting_photo, photo_sensor, temp_sensor, time_start,
+                 red_led, blue_led, args):
+        self.center_button = center_button
+        self.debounce_start = debounce_start
+        self.is_outputting_photo = is_outputting_photo
+        self.photo_sensor = photo_sensor
+        self.temp_sensor = temp_sensor
+        self.time_start = time_start
+        self.red_led = red_led
+        self.blue_led = blue_led
+        self.args = args
+
+    def set_outputting_photo(self, is_outputting_photo):
+        self.is_outputting_photo = is_outputting_photo
+
+    def set_debounce_start(self, debounce_start):
+        self.debounce_start = debounce_start
+
+    def set_time_start(self, time_start):
+        self.time_start = time_start
--- sensor_data_collector.py
+++ sensor_data_collector.py
@@ -7,15 +7,18 @@ except:
     from pyfirmata import Arduino, util
 
 from docopt import docopt
-
 import time
 
+from MicroController import MicroController
+
 usage = """
-Usage: 
+Usage:
+    sensor_data_collector.py 
     sensor_data_collector.py start <interval>
 """
 
 args = docopt(usage)
+
 
 def main():
     board = Arduino("COM4")
@@ -29,39 +32,40 @@ def main():
     photo_sensor = board.get_pin('a:2:i')
     photo_sensor.enable_reporting()
     center_button = board.get_pin('d:13:i')
-    center_button.enable_reporting()
+    top_button = board.get_pin('d:12:i')
+    top_button.enable_reporting()
 
     is_outputting_photo = False
 
     time_start = time.time()
     debounce_start = time.time()
 
+    micro_controller = MicroController(center_button, debounce_start, is_outputting_photo, photo_sensor, temp_sensor,
+                                       time_start, red_led, blue_led, args)
+
     time.sleep(1)  # VERY IMPORTANT, NEEDS TO BE INCLUDED AT THE BEGINNING
     print("Ready!")
-    if args['start']:
+    if args['start'] or top_button.read() == 1:
         while True:
-            center_button_value = center_button.read()
-            # print(f"center_button_value: {center_button_value}")
-            if center_button_value != 0 and time.time() - debounce_start > 0.2:
-                is_outputting_photo = set_outputting_photo(is_outputting_photo, center_button_value)
-                debounce_start = time.time()
+            read_data(micro_controller)
 
-            red_led.write(1)
-            photo_value = photo_sensor.read()
-            temp_value = temp_sensor.read()
 
-            volt = 4.98
-            degrees_celsius = ((temp_value * volt) / 0.01) - 273.15
-            degrees_celsius = round(degrees_celsius, 2)
+def read_data(micro_controller):
+    center_button_value = micro_controller.center_button.read()
+    # print(f"center_button_value: {center_button_value}")
+    if center_button_value != 0 and time.time() - micro_controller.debounce_start > 0.2:
+        micro_controller.set_outputting_photo(set_outputting_photo(micro_controller.is_outputting_photo,
+                                                                   center_button_value))
+        micro_controller.set_debounce_start(time.time())
 
-            if time.time() - time_start > float(args['<interval>']):
-                time_start = time.time()
-                if is_outputting_photo:
-                    print(f"{time.strftime('%a %H:%M:%S')} - {photo_value} - {photo_value}")
-                else:
-                    print(f"{time.strftime('%a %H:%M:%S')} - {temp_value} - {degrees_celsius}")
+    photo_value = micro_controller.photo_sensor.read()
+    temp_value = micro_controller.temp_sensor.read()
 
-            red_led.write(0)
+    volt = 4.98
+    degrees_celsius = ((temp_value * volt) / 0.01) - 273.15
+    degrees_celsius = round(degrees_celsius, 2)
+
+    print_data(micro_controller, photo_value, temp_value, degrees_celsius)
 
 
 def set_outputting_photo(is_outputting_photo, center_button_value):
@@ -70,6 +74,20 @@ def set_outputting_photo(is_outputting_photo, center_button_value):
             return False
         else:
             return True
+
+
+def print_data(micro_controller, photo_value, temp_value, degrees_celsius):
+    if time.time() - micro_controller.time_start > float(args['<interval>']):
+        micro_controller.set_time_start(time.time())
+        if micro_controller.is_outputting_photo:
+            micro_controller.red_led.write(1)
+            print(f"{time.strftime('%a %H:%M:%S')} - {photo_value} - {photo_value}")
+            micro_controller.red_led.write(0)
+
+        else:
+            micro_controller.blue_led.write(1)
+            print(f"{time.strftime('%a %H:%M:%S')} - {temp_value} - {degrees_celsius}")
+            micro_controller.blue_led.write(0)
 
 
 if __name__ == '__main__':
